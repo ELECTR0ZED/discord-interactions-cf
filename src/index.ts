c1f4c6bc966f7ac03220c6dfb2a9e740497484b3
ELECTR0ZED <ELECTR0ZED@users.noreply.github.com>
2025-03-06 10:04:31 +0000

    Add custom SlashCommandBuilder with asynchronous execution support

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
 import Client from './client/client';
+import { SlashCommandBuilder as OriginalSlashCommandBuilder } from '@discordjs/builders';
 
 export { Client };
 
 export * from '@discordjs/builders';
+
+class SlashCommandBuilder extends OriginalSlashCommandBuilder {
+    private executeFunction: ((interaction: any) => Promise<void>) | null = null;
+
+    setExecute(fn: (interaction: any) => Promise<void>) {
+        if (fn.constructor.name !== 'AsyncFunction') {
+            throw new Error('Execute function must be asynchronous');
+        }
+        this.executeFunction = fn;
+        return this;
+    }
+
+    async execute(interaction: any) {
+        if (this.executeFunction) {
+            await this.executeFunction(interaction);
+        } else {
+            throw new Error('No execute function set');
+        }
+    }
+}
+
+export { SlashCommandBuilder };
